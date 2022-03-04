145bed0a4eaeb80a7318fa6d54043162cc5e37e1
Andres <adas5@gcloud.ua.es>
2022-03-04 13:59:34 +0100

    Agregar archivos de proyecto.

---
+++ Jugador.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    class Jugador
+    {
+        private int maxAmonestaciones
+        {
+            get; set;
+        }
+        private int maxFaltas
+        {
+            get; set;
+        }
+
+        private int minEnergia
+        {
+            get; set;
+        }
+ 
+        private int amonestaciones
+        {
+            get
+            {
+                return amonestaciones;
+            }
+            set
+            {
+                if(maxAmonestaciones <= value)
+                {
+                    throw new amonestacionesMaximoExcedido();
+                }
+                else
+                {
+                    if (value < 0)
+                    {
+                        amonestaciones = 0;
+                    }
+                    else
+                    {
+                        amonestaciones = value;
+                    }
+                   
+                }
+            }
+        }
+        
+    }
+}

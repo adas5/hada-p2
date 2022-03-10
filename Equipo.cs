cbb60d2c27b4342b33764c9adbfa5d978966c6cf
Andres <adas5@gcloud.ua.es>
2022-03-10 20:31:25 +0100

    crear Equipo y sus variables 23849148B

---
+++ Equipo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+
+{
+    class Equipo
+    {
+        public int minJugadores { get; set; }
+        public int maxNumeroMovimientos { get; set; }
+        public int movimientos { get; private set; }
+        public string nombreEquipo { get; private set; }
+
+        public Equipo(int nj, string nom)
+        {
+            nombreEquipo = nom;
+            for(int i=0; i< nj; i++){
+                string nombre = "jugador_" + i;
+
+            }
+        }
+    }
+}
